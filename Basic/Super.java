21191a9cc2c817c034493efabe1faa70c7bab3a8
Riddhi Mistry <202201238@daiict.ac.in>
2023-09-13 14:35:02 +0530

    abcd

--- Basic/Super.java
+++ Basic/Super.java
@@ -1,12 +1,13 @@
-class Vehicle{
-    void manufacture(){
+class Vehicle {
+    void manufacture() {
         System.out.println("(P)Store Common manufacturing steps....");
     }
 }
-class Car extends Vehicle{
-    void manufacture(){
-            super.manufacture();
-           System.out.println("(C)Store Common manufacturing steps....");
+
+class Car extends Vehicle {
+    void manufacture() {
+        super.manufacture();
+        System.out.println("(C)Store Common manufacturing steps....");
     }
 }
 
@@ -14,12 +15,15 @@ public class Super {
     public static void main(String[] args) {
         Car c = new Car();
         c.manufacture();
-    }  
+    }
 }
 
 /*
-
-Scenario 1: Car Manufacturing
-In a car manufacturing system, have a base class called "Vehicle" with a method called "manufacture" that handles common manufacturing steps. Implement a derived class called "Car" that overrides the "manufacture" method and uses the super keyword to invoke the base class implementation.
-
+ * 
+ * Scenario 1: Car Manufacturing
+ * In a car manufacturing system, have a base class called "Vehicle" with a
+ * method called "manufacture" that handles common manufacturing steps.
+ * Implement a derived class called "Car" that overrides the "manufacture"
+ * method and uses the super keyword to invoke the base class implementation.
+ * --random change
  */
